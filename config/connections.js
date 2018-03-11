38c36bdd193c054b51b67d660ec570f4ec59254a
jeremy <jeremyfroment@yahoo.fr>
2018-03-11 14:25:20 +0100

    config mysql

--- config/connections.js
+++ config/connections.js
@@ -50,7 +50,7 @@ module.exports.connections = {
 
     mysqlServer: {
         adapter: 'sails-mysql',
-        host: '127.0.0.1',
+        host: 'localhost',
         user: 'root',
         password: 'root',
         database: 'libertyprim',
